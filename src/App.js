efc676e36a37916371dc1603389a4e3206c19480
Younes Belkouchi <belkouchiyounes@gmail.com>
2019-09-09 14:56:24 +0200

    Added some style sheets and the sidebar component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
 import React from 'react';
 import logo from './logo.svg';
+
+import Sidebar from "./components/sidebar";
+
+import './css/bootstrap.min.css';
+import './css/style.css';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App" id="colorlib-page">
+            <Sidebar
+                photo="Insert Photo"
+                name="Younes Belkouchi"
+                title="Machine Learning and Software Engineer"/>
+            <header className="App-header" id="colorlib-main">
+                <img src={logo} className="App-logo" alt="logo"/>
+                <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+            </header>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/sidebar.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import "../css/style.css";
+import "../css/bootstrap.min.css";
+
+class Sidebar extends React.Component {
+    render() {
+        return (
+            <aside class="border js-fullheight" id="sidebar">
+                <div className="text-center">
+                    <div className="author-img">{this.props.photo}</div>
+                    <h1 id="colorlib-logo">{this.props.name}</h1>
+                    <span className="position">{this.props.title}</span>
+                </div>
+                <nav id="colorlib-main-menu" className="navbar">
+                    <div id="navbar" className="collapse in" aria-expanded="true">
+                        <ul>
+                            <li className="active"><a href="#">Home</a></li>
+                            <li><a href="#">About</a></li>
+                            <li><a href="#">Services</a></li>
+                            <li><a href="#">Skills</a></li>
+                            <li><a href="#">Education</a></li>
+                            <li><a href="#">Experience</a></li>
+                            <li><a href="#">Work</a></li>
+                            <li><a href="#">Blog</a></li>
+                            <li><a href="#">Contact</a></li>
+                        </ul>
+                    </div>
+                </nav>
+            </aside>
+        )
+    }
+}
+
+export default Sidebar;
