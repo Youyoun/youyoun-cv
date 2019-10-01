f3a69a524b6fffb22e2092b88e1068b84cadb077
Younes Belkouchi <belkouchiyounes@gmail.com>
2019-10-01 23:56:22 +0200

    Updates

---
+++ precache-manifest.37c8cd60bc3003fc7a7003f24e35014d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "868426a96aeca410017d5ec346d1856f",
+    "url": "/youyoun-cv/index.html"
+  },
+  {
+    "revision": "1c2ac7af57bab01e6548",
+    "url": "/youyoun-cv/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "3f5581f08363da01a52b",
+    "url": "/youyoun-cv/static/css/main.f697a5c4.chunk.css"
+  },
+  {
+    "revision": "1c2ac7af57bab01e6548",
+    "url": "/youyoun-cv/static/js/2.e9235f9c.chunk.js"
+  },
+  {
+    "revision": "3f5581f08363da01a52b",
+    "url": "/youyoun-cv/static/js/main.17cc73ef.chunk.js"
+  },
+  {
+    "revision": "97a968e60d85e32eac5a",
+    "url": "/youyoun-cv/static/js/runtime~main.47469919.js"
+  },
+  {
+    "revision": "53621746d562f9b6c21854d3187f296d",
+    "url": "/youyoun-cv/static/media/linkedin.53621746.jpeg"
+  }
+]);
