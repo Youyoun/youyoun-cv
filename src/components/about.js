ff32a4ffe743d457120bd2ee203c4cbb01c4f2a5
Youyoun <belkouchiyounes@gmail.com>
2022-11-08 14:32:54 +0100

    Refactored the about code for better code structuring and a bit of css adjustement for less spacing

--- src/components/about.js
+++ src/components/about.js
@@ -7,66 +7,59 @@ import "./about.css"
 
 function AboutDescription() {
     return (
-        <section className="about">
-            <Row>
-                <Col md={12}>
-                    <span className="heading-meta">About Me</span>
-                    <h2 className="heading">Introduction</h2>
-                    <p>
-                        I graduated from Ecole Centrale Paris, a leading french engineering school.
-                        I have always been passionate about IT. I am currently taking a huge interest in everything
-                        that is related to machine learning or deep learning, and especially their application to the
-                        medical field. I am currently doing a PhD on deep learning methods applied to medical imaging.
-                    </p>
-                    <p>
-                        Beside my adoration for technology, I also enjoy playing video games and indoor climbing.
-                    </p>
-                </Col>
-            </Row>
-        </section>
+        <Row>
+            <Col md={12}>
+                <span className="heading-meta">About Me</span>
+                <h2 className="heading">Introduction</h2>
+            </Col>
+            <p>
+                I graduated from Ecole Centrale Paris, a leading french engineering school.
+                I have always been passionate about IT. I am currently taking a huge interest in everything
+                that is related to machine learning or deep learning, and especially their application to the
+                medical field. I am currently doing a PhD on deep learning methods applied to medical imaging.<br/>
+
+                Beside my adoration for technology, I also enjoy playing video games and indoor climbing.
+            </p>
+        </Row>
     )
 }
 
 function AboutExpertise() {
     return (
-        <section className="about">
-            <Row>
-                <Col md={6}>
-                    <h2 className="heading">Domains of interest</h2>
-                </Col>
-            </Row>
-            <Row>
-                <Pannel colorId={5} title="Machine Learning" icon="database">
-                    <p>
-                        I have worked on personal and professional projects that use machine learning and/or deep
-                        learning
-                        to answer some real world problems<br/>
-                        I enjoy this domain for the technical difficulties that arise when solving said problems.
-                    </p>
-                </Pannel>
-                <Pannel colorId={1} title="Fullstack Development" icon="terminal">
-                    <p>
-                        I have followed a tech intensive track called the digital tech year during which I honed my
-                        Fullstack
-                        Development skills. <br/>
-                        I have experience developing frontend and backend using multiple tools such as python or
-                        javascript.
-                        I developed this website using ReactJS.
-                        I find development fun and complementary to a lot of problems that relate to data science.
-                    </p>
-                </Pannel>
-                <Pannel colorId={3} title="Data Structures & Algorithms" icon="omega">
-                    <p>
-                        Since I studied computer science, I have a good grasp over concepts related to algorithmic,
-                        data structure or mathematics. <br/>
-                        I try to keep a hybrid profile where I am able to comprehend mathematics but also develop
-                        software based
-                        on these theories. My current algorithmic interests are related to mathematical optimization.
-                        I enjoy participating in coding contests to hone and practice skills related to this field.
-                    </p>
-                </Pannel>
-            </Row>
-        </section>
+        <Row>
+            <Col md={12}>
+                <h2 className="heading">Domains of interest</h2>
+            </Col>
+            <Pannel colorId={5} title="Machine Learning" icon="database">
+                <p>
+                    I have worked on personal and professional projects that use machine learning and/or deep
+                    learning
+                    to answer some real world problems<br/>
+                    I enjoy this domain for the technical difficulties that arise when solving said problems.
+                </p>
+            </Pannel>
+            <Pannel colorId={1} title="Fullstack Development" icon="terminal">
+                <p>
+                    I have followed a tech intensive track called the digital tech year during which I honed my
+                    Fullstack
+                    Development skills. <br/>
+                    I have experience developing frontend and backend using multiple tools such as python or
+                    javascript.
+                    I developed this website using ReactJS.
+                    I find development fun and complementary to a lot of problems that relate to data science.
+                </p>
+            </Pannel>
+            <Pannel colorId={3} title="Data Structures & Algorithms" icon="omega">
+                <p>
+                    Since I studied computer science, I have a good grasp over concepts related to algorithmic,
+                    data structure or mathematics. <br/>
+                    I try to keep a hybrid profile where I am able to comprehend mathematics but also develop
+                    software based
+                    on these theories. My current algorithmic interests are related to mathematical optimization.
+                    I enjoy participating in coding contests to hone and practice skills related to this field.
+                </p>
+            </Pannel>
+        </Row>
     )
 }
 
@@ -84,7 +77,6 @@ function Pannel(props) {
                     </div>
                 </div>
             </div>
-
         </Col>
     )
 }
@@ -92,8 +84,12 @@ function Pannel(props) {
 export default function About() {
     return (
         <div>
-            <AboutDescription/>
-            <AboutExpertise/>
+            <section className="about">
+                <Col md={12}>
+                    <AboutDescription/>
+                    <AboutExpertise/>
+                </Col>
+            </section>
         </div>
     )
 }
