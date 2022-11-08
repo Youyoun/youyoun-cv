d601f193936f61b5fac776c0a2192d64a550d415
Youyoun <belkouchiyounes@gmail.com>
2022-11-08 17:06:35 +0100

    Refactored sidebar: changed components to functional and used hooks + implemented changes to the section object in the sidebar

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -1,65 +1,47 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import Button from "react-bootstrap/Button";
 import "./sidebar.css"
 
 const scrollTo = ele => {
-  ele.scrollIntoView({
-    behavior: "smooth",
-    block: "start"
-  });
+    ele.scrollIntoView({
+        behavior: "smooth",
+        block: "start"
+    });
 };
 
-class NavButton extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      active: false,
-    }
-  }
-
-  render() {
+function NavButton(props) {
+    // Need to fix the problem of using li and button, feel not reactjsy
     return (
-      <li onFocus={() => this.setState({active: true})} onBlur={() => this.setState({active: false})}
-          className={this.props.selectedSection === this.props.section ? "active" : ""}>
-        <button className={"linkButton"} onClick={() => this.props.onClick()}>
-          {this.props.section}
-        </button>
-      </li>
+        <li className={`sidebar-menu-button ${props.selectedSection === props.section ? "active" : ""}`}>
+            <button className={"linkButton"} onClick={() => props.onClick()}>
+                {props.section}
+            </button>
+        </li>
     )
-  }
 }
 
-class Sidebar extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      open: false,
-    }
-  }
+function Sidebar(props) {
+    const [isOpen, setIsOpen] = useState(false);
 
-  handleClick() {
-    this.setState({open: !this.state.open})
-  }
-
-  render() {
     return (
-      <aside className={`sidebar ${this.state.open ? "open" : ""}`} id="sidebar">
-        <Button variant="outline-primary" onClick={() => this.handleClick()}
-                className={`glyphicon glyphicon-align-justify ${this.state.open ? "active" : ""}`}/>
-        <img className="round-img" src={this.props.photo}/>
-        <h1 id="sidebar-name">{this.props.name}</h1>
-        <span className="sidebar-job position">{this.props.title}</span>
-        <nav id="sidebar-menu" className="navbar">
-          <ul>
-            {Object.keys(this.props.sections).map((key, index) => <NavButton
-              onClick={() => scrollTo(this.props.sections[key].current)} key={index} section={key}
-              selectedSection={this.props.visibleSection}/>)}
-          </ul>
-        </nav>
-      </aside>
+        <aside className={`sidebar ${isOpen ? "open" : ""}`} id="sidebar">
+            <Button variant="outline-primary" onClick={() => setIsOpen(!isOpen)}
+                    className={`glyphicon glyphicon-align-justify ${isOpen ? "active" : ""}`}/>
+            <img className="round-img" src={props.photo} alt={""}/>
+            <h1 id="sidebar-name">{props.name}</h1>
+            <span className="sidebar-job position">{props.title}</span>
+            <nav id="sidebar-menu" className="navbar">
+                <ul>
+                    {props.sections.map(
+                        (element, index) => <NavButton onClick={() => scrollTo(element.ref.current)} key={index}
+                                                       section={element.section}
+                                                       selectedSection={props.visibleSection}/>
+                    )}
+                </ul>
+            </nav>
+        </aside>
     )
-  }
 }
 
 export default Sidebar;
