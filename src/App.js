698aff8afa2d2cdf16689005127d2bcb4a485661
Youyoun <belkouchiyounes@gmail.com>
2022-11-08 17:02:51 +0100

    (WIP) Refactored App.js:
    - Linting and formating
    - Set initial value of visibleSection to first element of sections
    - Refactored the sections objects to an array of objects
    - Changed for loop in handleScrolle to functional .find()

--- src/App.js
+++ src/App.js
@@ -8,109 +8,90 @@ import About from "./components/about";
 import Education from "./components/education";
 import Experience from "./components/experience";
 import Skills from "./components/skills";
-import Contact from "./components/contact";
 
 import photo from './linkedin.jpeg'
 
 function getDimensions(ele) {
-  const {height} = ele.getBoundingClientRect();
-  const offsetTop = ele.offsetTop;
-  const offsetBottom = offsetTop + height;
+    const {height} = ele.getBoundingClientRect();
+    const offsetTop = ele.offsetTop;
+    const offsetBottom = offsetTop + height;
 
-  return {
-    height,
-    offsetTop,
-    offsetBottom,
-  };
-};
-
-function scrollTo(ele) {
-  ele.scrollIntoView({
-    behavior: "smooth",
-    block: "start",
-  });
-};
+    return {
+        height,
+        offsetTop,
+        offsetBottom,
+    };
+}
 
 function App() {
-  const [visibleSection, setVisibleSection] = useState(null);
+    const introductionRef = useRef(null);
+    const aboutRef = useRef(null);
+    const experienceRef = useRef(null);
+    const educationRef = useRef(null);
+    const skillsRef = useRef(null);
 
-  const introductionRef = useRef(null);
-  const aboutRef = useRef(null);
-  const experienceRef = useRef(null);
-  const educationRef = useRef(null);
-  // const projectRef = useRef(null);
-  const skillsRef = useRef(null);
-  const contactRef = useRef(null);
+    const sections = [
+        {section: "Introduction", ref: introductionRef},
+        {section: "About", ref: aboutRef},
+        {section: "Experience", ref: experienceRef},
+        {section: "Education", ref: educationRef},
+        {section: "Skills", ref: skillsRef},
+    ];
 
-  const sections = {
-    "Introduction": introductionRef,
-    "About": aboutRef,
-    "Experience": experienceRef,
-    "Education": educationRef,
-    "Skills": skillsRef,
-    //"Contact": contactRef,
-  };
+    const [visibleSection, setVisibleSection] = useState(sections[0].section);
 
-  useEffect(() => {
-    function handleScroll() {
-      const scrollPosition = window.scrollY;
 
-      let selected;
-      for (var section in sections) {
-        const ele = sections[section].current;
-        if (ele) {
-          const {offsetBottom, offsetTop} = getDimensions(ele);
-          if (scrollPosition >= (offsetTop > 0 ? offsetTop - 300 : offsetTop) && scrollPosition < offsetBottom - 300) {
-            selected = section;
-            break
-          }
+    useEffect(() => {
+        const handleScroll = () => {
+            const scrollPosition = window.scrollY;
+            let selected = sections.find(({_, ref}) => {
+                const ele = ref.current;
+                if (ele) {
+                    const {offsetBottom, offsetTop} = getDimensions(ele);
+                    return scrollPosition >= (offsetTop > 0 ? offsetTop - 300 : offsetTop) && scrollPosition < offsetBottom - 300
+                }
+                return false;
+            })
+            if (selected.section !== visibleSection) {
+                setVisibleSection(selected.section);
+            } else if (!selected.section && visibleSection) {
+                setVisibleSection(null);
+            }
         }
-      }
-      if (selected !== visibleSection) {
-        setVisibleSection(selected);
-      } else if (!selected && visibleSection) {
-        setVisibleSection(null);
-      }
-    };
 
-    handleScroll();
-    window.addEventListener("scroll", handleScroll);
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, [visibleSection, sections]);
-  return (
-    <div>
-      <Sidebar
-        sections={sections}
-        photo={photo}
-        name="Younes Belkouchi"
-        title="Machine Learning and Software Engineer"
-        visibleSection={visibleSection}/>
-      <div id="app">
-        <div ref={introductionRef}>
-          <Introduction/>
+        window.addEventListener("scroll", handleScroll);
+        return () => {
+            window.removeEventListener("scroll", handleScroll);
+        };
+    }, [visibleSection, sections]);
+    return (
+        <div>
+            <Sidebar sections={sections}
+                     photo={photo}
+                     name="Younes Belkouchi"
+                     title="Machine Learning and Software Engineer"
+                     visibleSection={visibleSection}/>
+            <div id="app">
+                <div ref={introductionRef}>
+                    <Introduction/>
+                </div>
+                <div className="pad-sides">
+                    <div ref={aboutRef}>
+                        <About/>
+                    </div>
+                    <div ref={experienceRef}>
+                        <Experience/>
+                    </div>
+                    <div ref={educationRef}>
+                        <Education/>
+                    </div>
+                    <div ref={skillsRef}>
+                        <Skills/>
+                    </div>
+                </div>
+            </div>
         </div>
-        <div className="pad-sides">
-          <div ref={aboutRef}>
-            <About/>
-          </div>
-          <div ref={experienceRef}>
-            <Experience/>
-          </div>
-          <div ref={educationRef}>
-            <Education/>
-          </div>
-          <div ref={skillsRef}>
-            <Skills/>
-          </div>
-          {/*<div ref={contactRef}>*/}
-          {/*  <Contact/>*/}
-          {/*</div>*/}
-        </div>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default App;
