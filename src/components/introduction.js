7285613c4ed9c6bcd4ba0bf48bc93babd89e5e53
Youyoun <belkouchiyounes@gmail.com>
2022-11-08 19:29:11 +0100

    Refactored introduction to use hooks and cleaned the Carousel code

--- src/components/introduction.js
+++ src/components/introduction.js
@@ -1,113 +1,90 @@
-import React, {Component} from 'react';
+import React, {useEffect, useState} from 'react';
 import Container from "react-bootstrap/Container";
 import Col from "react-bootstrap/Col";
 import IcoMoon from "react-icomoon";
 import "./Introduction.css";
 
 function Slide({background, children}) {
-  return (
-    <div className={`slide`} style={{backgroundImage: background}}>
-      <Container className={"no-margin-left"}>
-        <Col xs={12} md={8}>
-          {children}
-        </Col>
-      </Container>
-    </div>
-  )
+    return (
+        <div className={`slide`} style={{backgroundImage: background}}>
+            <Container className={"no-margin-left"}>
+                <Col xs={12} md={8}>
+                    {children}
+                </Col>
+            </Container>
+        </div>
+    )
 }
 
 function Button(props) {
-  let icon = props.iconClass ? <IcoMoon icon={props.iconClass}/> : null;
-  return (
-    <a className="btn btn-primary button" id={props.id}
-       href={props.link}
-       target="_blank" rel="noopener noreferrer" style={props.style}>
-      {props.text} {icon}
-    </a>
-  )
-}
-
-class Carousel extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      n_slides: 0,
-      current_slide: 0,
-      slides: [null],
-      start: Date.now(),
-      timer: 0,
-      interval: 10,
-    }
-  }
-
-  componentDidMount() {
-    this.setState({
-      slides: this.props.children,
-      n_slides: this.props.children.length,
-      interval: this.props.autoSwapDelay,
-      fadeEffect: ""
-    });
-    this.startTimer();
-  }
-
-  nextSlide() {
-    this.setState({fadeEffect: "fadeOut"}, () => {
-        setTimeout(() => this.setState({
-          current_slide: (this.state.current_slide + 1) % this.state.n_slides,
-          fadeEffect: "fadeIn"
-        }), 2000)
-      }
-    )
-  }
-
-  startTimer() {
-    this.timer = setInterval(() => this.setState({
-      timer: Date.now() - this.state.start
-    }, () => this.swapSlideOnIntervalPass()), 1000)
-  }
-
-  swapSlideOnIntervalPass() {
-    if (Math.floor(this.state.timer / 1000) % this.state.interval === 0) {
-      this.nextSlide();
-    }
-  }
-
-  render() {
+    let icon = props.iconClass ? <IcoMoon icon={props.iconClass}/> : null;
     return (
-      <div className={`slide-container ${this.state.fadeEffect}`}>
-        {this.state.slides[this.state.current_slide]}
-      </div>
+        <a className="btn btn-primary button" id={props.id}
+           href={props.link}
+           target="_blank" rel="noopener noreferrer" style={props.style}>
+            {props.text} {icon}
+        </a>
     )
-  }
 }
 
-export default class Introduction extends Component {
-  render() {
+function Carousel(props) {
+    const delayTimeInMiliseconds = 1000
+
+    const [currentSlide, setCurrentSlide] = useState(0)
+    const [timer, setTimer] = useState(0)
+
+
+    useEffect(() => {
+        const start = Date.now()
+        const startTimer = () => {
+            setInterval(() => setTimer(Date.now() - start), delayTimeInMiliseconds)
+        }
+        startTimer()
+    }, [])
+
+
+    useEffect(() => {
+        const swapSlideOnIntervalPass = () => {
+            if (Math.floor(timer / delayTimeInMiliseconds) % props.autoSwapDelay === props.autoSwapDelay - 1) {
+                setCurrentSlide(currentSlide => (currentSlide + 1) % props.children.length)
+            }
+        }
+        swapSlideOnIntervalPass()
+    }, [timer, props.autoSwapDelay, props.children.length])
+
     return (
-      <section id="introduction">
-        <Carousel autoSwapDelay={7}>
-          <Slide background="url(images/intro-1.jpg)">
-            <h1 id={"slide1-text"}>Hello! <br/>I'm Younes</h1>
-            <Button
-              id="slide1-button"
-              link="CV_younes_belkouchi.pdf"
-              text="View CV" iconClass="folder-download"/>
-          </Slide>
-          <Slide background="url(images/youyounDTY.jpg)">
-            <h1 style={{color: "white"}}>Check out some of my projects !</h1>
-            <Button
-              id="slide1-button"
-              link="https://github.com/youyoun"
-              text="View Projects" iconClass="briefcase"/>
-            <Button
-              id="slide1-button"
-              style={{marginLeft: '5px'}}
-              link="https://scholar.google.com/citations?user=06I0Q1sAAAAJ"
-              text="View Research" iconClass="briefcase"/>
-          </Slide>
-        </Carousel>
-      </section>
+        <div className={`slide-container`}>
+            {props.children[currentSlide]}
+        </div>
     )
-  }
 }
+
+export default function Introduction() {
+    return (
+        <section id="introduction">
+            <Carousel autoSwapDelay={7}>
+                <Slide background="url(images/intro-1.jpg)">
+                    <h1 id={"slide1-text"}>Hello! <br/>I'm Younes</h1>
+                    <Button
+                        id="slide1-button"
+                        link="CV_younes_belkouchi.pdf"
+                        text="View CV" iconClass="folder-download"/>
+                </Slide>
+                <Slide background="url(images/youyounDTY.jpg)">
+                    <h1 style={{color: "white"}}>Check out some of my projects !</h1>
+                    <Button
+                        id="slide1-button"
+                        link="https://github.com/youyoun"
+                        text="View Projects" iconClass="briefcase"/>
+                    <Button
+                        id="slide1-button"
+                        style={{marginLeft: '5px'}}
+                        link="https://scholar.google.com/citations?user=06I0Q1sAAAAJ"
+                        text="View Research" iconClass="briefcase"/>
+                </Slide>
+            </Carousel>
+        </section>
+    )
+}
+
 
