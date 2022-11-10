e032368c5d88b0bf50136188349e36157ed4f5bc
Youyoun <belkouchiyounes@gmail.com>
2022-11-10 18:55:44 +0100

    Removed unused imports

--- src/components/introduction.js
+++ src/components/introduction.js
@@ -1,4 +1,4 @@
-import React, {useCallback, useEffect, useMemo, useRef, useState} from 'react';
+import React, {useCallback, useEffect, useState} from 'react';
 import Container from "react-bootstrap/Container";
 import Col from "react-bootstrap/Col";
 import IcoMoon from "react-icomoon";
